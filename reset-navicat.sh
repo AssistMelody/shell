711eb75c1fff167852cbfe4bed0db1cd20e6aab8
yuan.shen <yuan.shen@marketin.cn>
2023-11-27 17:21:26 +0800

    更新sh

--- reset-navicat.sh
+++ reset-navicat.sh
@@ -14,8 +14,12 @@
 # @raycast.author Yuanshen
 # @raycast.authorURL https://github.com/AssistMelody/shell.git
 
+echo "*******删除试用时间*****"
+
+defaults delete com.navicat.NavicatPremium 91F6C435D172C8163E0689D3DAD3F3E9
+defaults delete com.navicat.NavicatPremium B966DBD409B87EF577C9BBF3363E9614
+rm -rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/
+
 /usr/libexec/PlistBuddy -c "Delete :91F6C435D172C8163E0689D3DAD3F3E9" ~/Library/Preferences/com.navicat.NavicatPremium.plist
 /usr/libexec/PlistBuddy -c "Delete :B966DBD409B87EF577C9BBF3363E9614" ~/Library/Preferences/com.navicat.NavicatPremium.plist
-
 rm -rf ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.*
-
