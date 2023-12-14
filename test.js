6610c2d70e97eecb76c25e544b0cc43b4cc32bc3
yuan.shen <yuan.shen@marketin.cn>
2023-12-14 18:47:28 +0800

    添加raycast 配置备份

---
+++ test.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env /Users/marketin/.volta/bin/node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title test
+// @raycast.mode silent
+
+// Optional parameters:
+// @raycast.icon 🤖
+
+// Documentation:
+// @raycast.description hahahah
+// @raycast.author Yuanshen
+// @raycast.authorURL https://github.com/AssistMelody/shell.git
+
+console.log(process.pwd());
